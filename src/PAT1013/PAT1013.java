8627bb68ee70a484f0999d404258b697ac0f01f5
Xiny <415589016@qq.com>
2015-01-30 17:02:17 +0800

    1010 7th case wrong, maybe too big

--- src/PAT1013/PAT1013.java
+++ src/PAT1013/PAT1013.java
@@ -1,7 +1,24 @@
 package PAT1013;
 
+import java.util.Scanner;
+
 /**
  * Created by xiny on 2015/1/30.
  */
 public class PAT1013 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int N,M,K;
+        N = in.nextInt();
+        M = in.nextInt();
+        K = in.nextInt();
+        int[][] map = new int[N][N];
+        int x,y;
+        for(int i=0;i<M;i++){
+            x = in.nextInt();
+            y = in.nextInt();
+            map[x][y] = 1;
+        }
+
+    }
 }
